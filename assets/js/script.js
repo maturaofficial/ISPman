8ad74efb248b7bf8cf18a6bde723f3df86985c05
maturaofficial <maturaofficial@gmail.com>
2022-11-24 11:57:26 -0800

    Malapit na ako mag present ito palang ginawa ko

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,19 @@
 // Initialized AOS
 AOS.init();
 
+function checkScroll(){
+    var startY = $('.navbar').height() * 1; //The point where the navbar changes in px
 
+    if($(window).scrollTop() > startY){
+        $('.navbar').addClass("scrolled");
+    }else{
+        $('.navbar').removeClass("scrolled");
+    }
+}
+
+if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+        checkScroll();
+    });
+}
      
